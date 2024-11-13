a5483d43a4650b6cf9afcdabe7cee4c42f3cc778
Bryan Francisco <franciscobryaaan@gmail.com>
2024-11-13 14:38:32 +0800

    initial booking widgets

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
 import streamlit as st
+import datetime
 
 st.title("Magis Air ✈️")
+
+temp_cities = [
+    "Manila",
+    "Gensan",
+    "Cebu",
+]
+
+origin = st.selectbox(
+    "Select origin",
+    (temp_cities)
+)
+
+destination = st.selectbox(
+    "Select destination",
+    (temp_cities)
+)
+
+departure_date = st.date_input("Select preferred departure date", value=None)
+
+departure_time = st.time_input("Select preferred departure time", value=None)
+
+st.button("Check flights")
