dcfb6db33e55363e1fd8cfec214606241d74403e
Bryan <franciscobryaaan@gmail.com>
2024-11-29 01:23:19 +0800

    queries

--- pages/2_schedules.py
+++ pages/2_schedules.py
@@ -5,7 +5,7 @@ db = st.session_state["db"]
 
 # Filter flight schedules based on session_state data
 # sql query filter:
-#   - departure_date + 6days
+#   - departure_date + 6days (query per day)
 #   - pref_time + 3hrs
 #   - duration
 #   - origin
@@ -18,19 +18,53 @@ db = st.session_state["db"]
 #   - arrival_time
 #   - flight_code
 
+days = ["Mon", "Tues", "Wed", "Thur", "Fri", "Sat", "Sun"]
+day_ofthe_week = st.session_state["pref_date"].weekday()   # int. 0 - Monday, 1 - Tuesday, ...
+for i in range(7):
+    if day_ofthe_week == i:
+        break
+    days.append(days.pop(0))
+
+query = []
+for i in range(7):
+    where1 = f"DATE'{st.session_state['pref_date'] + datetime.timedelta(days=i)}' = departure_date"
+    time_obj = datetime.time(hour=(st.session_state['pref_time'].hour + 2)%24, minute=st.session_state['pref_time'].minute)
+    where2 = f"TIME'{time_obj}' <= departure_time"
+    where3 = f"city_origin LIKE '{st.session_state['origin']}' AND city_destination LIKE '{st.session_state['destination']}'"
+    db_cols = "departure_time, arrival_time, flight_cost"
+    query.append(
+        db.query(f"SELECT {db_cols} FROM flight_schedule fs LEFT JOIN route rt ON fs.route_id = rt.route_id WHERE {where1} AND {where2} AND {where3}").to_dict()
+    )
+
+query[0]
+print(query[0].items())
+for k, v in query[0].items():
+    st.write(k)
+    st.write(v)
+
+clean_query = [[] for i in range(7)]
+for i in range(7):
+    
+    for k, v_dict in query[i].items():
+        dct = {"departure_date" : None, "departure_time" : None, "arrival_time" : None, "flight_code" : None}
+        
+        for key in range(len(query[i]["departure_time"])):
+            dct[k]
+        
+    
+    v["departure_time"]["0"]
+
 temp_query = [
     [
         {
             "departure_date" : datetime.date(year=2024, month=1, day=1),
             "departure_time" : datetime.time(hour=1, minute=30),
-            "duration" : 120,
             "arrival_time" : datetime.time(hour=3, minute=30),
             "flight_code" : "ABC-001"
         },
         {
             "departure_date" : datetime.date(year=2024, month=1, day=1),
             "departure_time" : datetime.time(hour=3, minute=30),
-            "duration" : 120,
             "arrival_time" : datetime.time(hour=5, minute=30),
             "flight_code" : "ABC-002"
         },
@@ -39,7 +73,6 @@ temp_query = [
         {
             "departure_date" : datetime.date(year=2024, month=1, day=2),
             "departure_time" : datetime.time(hour=4, minute=0),
-            "duration" : 180,
             "arrival_time" : datetime.time(hour=7, minute=0),
             "flight_code" : "ABC-003"
         },
@@ -48,7 +81,6 @@ temp_query = [
         {
             "departure_date" : datetime.date(year=2024, month=1, day=2),
             "departure_time" : datetime.time(hour=4, minute=0),
-            "duration" : 60,
             "arrival_time" : datetime.time(hour=5, minute=0),
             "flight_code" : "ABC-004"
         },
@@ -64,13 +96,6 @@ with h_col2:
     if st.button("🔙 Return"):
         st.switch_page("pages/1_destination.py")
 
-days = ["Mon", "Tues", "Wed", "Thur", "Fri", "Sat", "Sun"]
-day_ofthe_week = st.session_state["pref_date"].weekday()   # int. 0 - Monday, 1 - Tuesday, ...
-for i in range(7):
-    if day_ofthe_week == i:
-        break
-    days.append(days.pop(0))
-
 pref_date = st.session_state["pref_date"]
 tabs = st.tabs([f'{days[i]}, {(pref_date + datetime.timedelta(days=i)).strftime("%b %d")}' for i in range(7)])
 
@@ -82,17 +107,17 @@ for i in range(7):
             st.write("😢 No available flights for this date.")
             continue
         
-        for db in db_day:
+        for db_query in db_day:
             with st.container(border=True):
                 cols = st.columns([4.7, 1, 1], vertical_alignment="center")
                 with cols[0]:
                     st.write(f'✈️ **{st.session_state["origin"]} to {st.session_state["destination"]}**')
                     
-                    st.write(f'🕒 {db["departure_time"].strftime("%H:%M")} - {db["arrival_time"].strftime("%H:%M")} UTC')
+                    st.write(f'🕒 {db_query["departure_time"].strftime("%H:%M")} - {db_query["arrival_time"].strftime("%H:%M")} UTC')
                 with cols[-2]:
                     st.write("PHP 2,000.00")
                 with cols[-1]:
-                    if st.button("🛫 Book", key=db["flight_code"]):
+                    if st.button("🛫 Book", key=db_query["flight_code"]):
                         # put relevant data to st.session_state
                         st.switch_page("pages/3_passenger.py")
 
