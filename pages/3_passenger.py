6abce1eeb36293645743b3fb5378b9b5330ab846
Bryan Francisco <franciscobryaaan@gmail.com>
2024-11-28 20:37:55 +0800

    done page 3

--- pages/3_passenger.py
+++ pages/3_passenger.py
@@ -1,3 +1,28 @@
 import streamlit as st
+import datetime
 
-st.write(st.session_state)
+h_col1, h_col2 = st.columns([5.77, 1], vertical_alignment="bottom")
+with h_col1:
+    st.header("Passenger Details")
+with h_col2:
+    if st.button("🔙 Return"):
+        st.switch_page("pages/2_schedules.py")
+
+name_cols = st.columns(3) 
+with name_cols[0]:
+    st.text_input("Last name")
+with name_cols[1]:
+    st.text_input("First name")
+with name_cols[2]:
+    st.text_input("Middle name")
+
+birth_date = st.date_input("Birthdate", value=datetime.datetime(2004, 1, 1))
+
+gender = st.selectbox(
+    "Gender",
+    ["Female", "Male", "Nonbinary", "Prefer not to say"],
+)
+
+if st.button("✅ Confirm", use_container_width=True):
+    # put data to session_state
+    st.switch_page("pages/4_booking.py")
