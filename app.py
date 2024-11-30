09c93fec56150db45f5bb68077bf97e7b0d48047
Bryan Francisco <franciscobryaaan@gmail.com>
2024-11-30 18:34:20 +0800

    done: page 5

--- app.py
+++ app.py
@@ -11,5 +11,6 @@ pg = st.navigation([
     st.Page("pages/2_schedules.py", title="Magis Air | Flight Schedules", icon="✈️"),
     st.Page("pages/3_passenger.py", title="Magis Air | Passenger Info", icon="✈️"),
     st.Page("pages/4_booking.py", title="Magis Air | Flight Booking", icon="✈️"),
+    st.Page("pages/5_summary.py", title="Magis Air | Booking Summary", icon="✈️"),
 ])
 pg.run()
---
+++ pages/5_summary.py
@@ -0,0 +1,54 @@
+import streamlit as st
+
+db = st.session_state["db"]
+
+name_cols = st.columns(3) 
+with name_cols[0]:
+    last_name = st.text_input("Last name")
+with name_cols[1]:
+    first_name = st.text_input("First name")
+with name_cols[2]:
+    middle_name = st.text_input("Middle name")
+
+if st.button("📃 Show Booked Flights", use_container_width=True):    
+    if last_name == "" or first_name == "" or middle_name == "":
+        st.error("Please fill out all fields.", icon="🚨")
+    else:
+        q_passenger_info = f"""
+        select pa.last_name as Last_Name, pa.first_name as First_Name, pa.middle_name as Middle_Name, pa.birth_date as Birthdate, pa.gender as Gender, bk.total_cost as Total_Cost
+        from booking bk
+        LEFT JOIN passenger pa ON bk.passenger_id=pa.passenger_id 
+        WHERE pa.first_name='{first_name}' AND pa.middle_name='{middle_name}' AND pa.last_name='{last_name}';
+        """
+
+        q_trip_itinerary = f"""
+        select fs.flight_code as Flight_Code, co.name as Origin, cd as Destination, fs.departure_time as Departure, fs.arrival_time as Arrival, fs.duration as Duration, fs.flight_cost as Cost
+        from booking bk 
+        LEFT JOIN passenger pa ON bk.passenger_id=pa.passenger_id 
+        LEFT JOIN flight_schedule fs ON bk.flight_code=fs.flight_code 
+        LEFT JOIN route rt ON fs.route_id=rt.route_id 
+        LEFT JOIN city co ON rt.city_origin=co.name 
+        LEFT JOIN city cd ON rt.city_destination=cd.name
+        LEFT JOIN booking_additional_item bkai ON bk.booking_id=bkai.booking_id
+        LEFT JOIN additional_item ai ON bkai.item_no=ai.item_no
+        WHERE pa.first_name='{first_name}' AND pa.middle_name='{middle_name}' AND pa.last_name='{last_name}';
+        """
+        q_addtl_items = f"""
+        select ai.item_name as Description, bkai.qty as Qty, (ai.cost*bkai.qty) as Cost 
+        from booking bk 
+        LEFT JOIN passenger pa ON bk.passenger_id=pa.passenger_id 
+        LEFT JOIN booking_additional_item bkai ON bk.booking_id=bkai.booking_id
+        LEFT JOIN additional_item ai ON bkai.item_no=ai.item_no
+        WHERE pa.first_name='{first_name}' AND pa.middle_name='{middle_name}' AND pa.last_name='{last_name}';
+        """
+
+        passenger_info = db.query(q_passenger_info)
+        trip_itinerary = db.query(q_trip_itinerary)
+        addtl_items = db.query(q_addtl_items)
+
+        st.subheader("Passenger Info")
+        passenger_info
+        st.subheader("Trip Itinerary")
+        trip_itinerary
+        st.subheader("Additional Items")
+        addtl_items
