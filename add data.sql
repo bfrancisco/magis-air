acb6010154f86fd3f2ae2319c5fc1771bd91b851
Gabriel Salvador <gabosalvador2004@gmail.com>
2024-12-02 17:28:20 +0800

    add flights for dec 4 onwards

---
+++ add data.sql
@@ -0,0 +1,45 @@
+INSERT INTO flight_schedule (departure_time, duration, flight_cost, departure_date, route_id) VALUES (
+    TIME '08:15:00', INTERVAL '2 Hours 15 Minutes', 1000, DATE '2024-12-04', 1
+);
+INSERT INTO flight_schedule (departure_time, duration, flight_cost, departure_date, route_id) VALUES (
+    TIME '09:30:00', INTERVAL '3 Hours', 1200, DATE '2024-12-05', 2
+);
+INSERT INTO flight_schedule (departure_time, duration, flight_cost, departure_date, route_id) VALUES (
+    TIME '12:45:00', INTERVAL '1 Hour 45 Minutes', 800, DATE '2024-12-06', 3
+);
+INSERT INTO flight_schedule (departure_time, duration, flight_cost, departure_date, route_id) VALUES (
+    TIME '15:20:00', INTERVAL '2 Hours 30 Minutes', 1500, DATE '2024-12-07', 4
+);
+INSERT INTO flight_schedule (departure_time, duration, flight_cost, departure_date, route_id) VALUES (
+    TIME '18:00:00', INTERVAL '4 Hours', 2000, DATE '2024-12-08', 5
+);
+INSERT INTO flight_schedule (departure_time, duration, flight_cost, departure_date, route_id) VALUES (
+    TIME '06:00:00', INTERVAL '3 Hours 15 Minutes', 1300, DATE '2024-12-09', 1
+);
+INSERT INTO flight_schedule (departure_time, duration, flight_cost, departure_date, route_id) VALUES (
+    TIME '07:45:00', INTERVAL '2 Hours', 1100, DATE '2024-12-10', 2
+);
+INSERT INTO flight_schedule (departure_time, duration, flight_cost, departure_date, route_id) VALUES (
+    TIME '14:10:00', INTERVAL '1 Hour 30 Minutes', 900, DATE '2024-12-11', 3
+);
+INSERT INTO flight_schedule (departure_time, duration, flight_cost, departure_date, route_id) VALUES (
+    TIME '20:30:00', INTERVAL '5 Hours', 2500, DATE '2024-12-12', 4
+);
+INSERT INTO flight_schedule (departure_time, duration, flight_cost, departure_date, route_id) VALUES (
+    TIME '10:20:00', INTERVAL '2 Hours 45 Minutes', 1400, DATE '2024-12-13', 5
+);
+INSERT INTO flight_schedule (departure_time, duration, flight_cost, departure_date, route_id) VALUES (
+    TIME '11:30:00', INTERVAL '3 Hours 30 Minutes', 1600, DATE '2024-12-14', 1
+);
+INSERT INTO flight_schedule (departure_time, duration, flight_cost, departure_date, route_id) VALUES (
+    TIME '17:45:00', INTERVAL '4 Hours 15 Minutes', 2100, DATE '2024-12-15', 2
+);
+INSERT INTO flight_schedule (departure_time, duration, flight_cost, departure_date, route_id) VALUES (
+    TIME '08:25:00', INTERVAL '2 Hours 10 Minutes', 1050, DATE '2024-12-16', 3
+);
+INSERT INTO flight_schedule (departure_time, duration, flight_cost, departure_date, route_id) VALUES (
+    TIME '13:35:00', INTERVAL '1 Hour 20 Minutes', 800, DATE '2024-12-17', 4
+);
+INSERT INTO flight_schedule (departure_time, duration, flight_cost, departure_date, route_id) VALUES (
+    TIME '21:00:00', INTERVAL '5 Hours 30 Minutes', 2700, DATE '2024-12-18', 5
+);
